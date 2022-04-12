bfe7003da77d0590f404f3f2d6f11a50bb6c8980
henry-jessica <s00214115@mail.itsligo.ie>
2022-04-12 15:01:45 +0100

    firebase settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from "../environments/environment";
 import { AppComponent } from './app.component';
 import { ListEventsComponent } from './list-events/list-events.component';
 import { EventComponent } from './event/event.component';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { EventComponent } from './event/event.component';
   imports: [
     BrowserModule, 
     HttpClientModule, 
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+
 export const environment = {
-  production: true
+  production: true, 
+  firebase: {
+    apiKey: "AIzaSyCWnzYJG9iJ3eaE5CGWPSiGLrjvcCnXeXU",
+    authDomain: "event-web-final-project.firebaseapp.com",
+    databaseURL: "https://event-web-final-project-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "event-web-final-project",
+    storageBucket: "event-web-final-project.appspot.com",
+    messagingSenderId: "806645342611",
+    appId: "1:806645342611:web:f87ff9f7bc1d2304b6c866"
+  }
 };
