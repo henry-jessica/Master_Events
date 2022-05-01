100914f55bd3583264c21c57a7bd297b87bd9e83
henry-jessica <s00214115@mail.itsligo.ie>
2022-05-01 22:11:02 +0100

    clean app component and add home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,27 +9,4 @@ import{IEvent} from './Interfaces/ticketmaster';
 })
 export class AppComponent {
 
-
-  eventData?: any; 
-  eventsData?:IEvent[]; 
-  errorMessage:any; 
-  
-  constructor(private _eventAPIService: TicketmasterApiService) { }
-  
-  getEventDetail(location:string):boolean{
-  this._eventAPIService.getEventData(location).subscribe(
-    eventData => {
-      this.eventData=eventData; 
-      this.eventsData = this.eventData._embedded.events; 
-      console.log('events', this.eventsData); 
-
-      this.eventsData?.forEach(element => {
-        console.log(element.name); 
-      });
-    }, 
-    error=> this.errorMessage = <any>error 
-  ); 
-  return false; 
-  }
-  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from "../environments/environment";
 import { AppComponent } from './app.component';
-// import { ListEventsComponent } from './components/list-events/list-events.component';
 import { EventComponent } from './components/event/event.component';
 import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { ListEventsComponent } from './components/list-events/list-events.component';
+import { SearchComponent } from './components/search/search.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './routing';
+import { AboutComponent } from './components/about/about.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventComponent, 
-    ListEventsComponent
+    ListEventsComponent, SearchComponent, EventDetailsComponent, HomeComponent, AboutComponent, 
   ],
   imports: [
     BrowserModule, 
     HttpClientModule, 
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireModule
+    AngularFireModule, 
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
