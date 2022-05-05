dbf780c0e2b4f4df0d3a0074f7353f24a82e09d3
henry-jessica <s00214115@mail.itsligo.ie>
2022-05-05 15:44:10 +0100

    all bugs fixed

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -1,67 +1,3 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { IEvent, Event} from 'src/app/Interfaces/ticketmaster';
-import { ActivatedRoute } from '@angular/router';
-import { TicketmasterApiService } from 'src/app/services/ticketmaster-api.service';
-import { faStar as farStar } from '@fortawesome/free-solid-svg-icons';
-import{ faStar as fasStar } from '@fortawesome/free-regular-svg-icons'
-
-@Component({
-  selector: 'app-event',
-  templateUrl: './event.component.html',
-  styleUrls: ['./event.component.scss']
-})
-export class EventComponent implements OnInit {
-  btnName?: string="Add to Favorite";
-
-farStar=farStar; 
-fasStar=fasStar; 
-
-star:any=this.farStar ;
-
-  constructor(private route:ActivatedRoute,   private _eventApiService?: TicketmasterApiService){
-    this.route.params
-    .subscribe(params=>console.log(params)); 
-  }
-
-  @Input() event?:IEvent; 
-  @Input() favorites?:boolean; 
-  //eventData?:IEvent; 
-
-  ngOnInit(): void {
-    if(this.favorites){
-      this.star=this.farStar; 
-      this.btnName="Remove"; 
-    }
-  
-  }
-  AddOrRemove(event?:IEvent):boolean{
-    if(this.favorites){
-      this.star=this.fasStar; 
-      this.DeleteFavorite(event); 
-    }
-    else{
-      this.AddToFavorite(event); 
-      this.star=this.farStar; 
-    }
-    return false; 
-  }
-
-  AddToFavorite(event?:IEvent): boolean{
-    let tempEvent:Event; 
-    tempEvent = new Event(event); 
-    this._eventApiService?.addEventData(tempEvent.event); 
-    return false;  
-  }
-  DeleteFavorite(event?:IEvent):boolean{
-    this._eventApiService?.delfavoriteData(event); 
-    return false; 
-  }
-
-
-}
-
-
-
 // import { Component, Input, OnInit } from '@angular/core';
 // import { IEvent, Event} from 'src/app/Interfaces/ticketmaster';
 // import { ActivatedRoute } from '@angular/router';
@@ -80,7 +16,8 @@ star:any=this.farStar ;
 // farStar=farStar; 
 // fasStar=fasStar; 
 
-// star:any=this.fasStar ;
+// star:any=this.farStar ;
+
 //   constructor(private route:ActivatedRoute,   private _eventApiService?: TicketmasterApiService){
 //     this.route.params
 //     .subscribe(params=>console.log(params)); 
@@ -122,5 +59,72 @@ star:any=this.farStar ;
 
 
 // }
+
+
+
+import { Component, Input, OnInit } from '@angular/core';
+import { IEvent, Event} from 'src/app/Interfaces/ticketmaster';
+import { ActivatedRoute } from '@angular/router';
+import { TicketmasterApiService } from 'src/app/services/ticketmaster-api.service';
+import { faStar as farStar } from '@fortawesome/free-solid-svg-icons';
+import{ faStar as fasStar } from '@fortawesome/free-regular-svg-icons'
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+  styleUrls: ['./event.component.scss']
+})
+export class EventComponent implements OnInit {
+  btnName?: string="Add to Favorite";
+
+farStar=farStar; 
+fasStar=fasStar; 
+
+star:any=this.fasStar ;
+  constructor(private route:ActivatedRoute,   private _eventApiService?: TicketmasterApiService){
+    this.route.params
+    .subscribe(params=>console.log(params)); 
+  }
+
+  @Input() event?:IEvent; 
+  @Input() favorites?:boolean; 
+  //eventData?:IEvent; 
+
+  ngOnInit(): void {
+    if(this.favorites){
+      this.star=this.fasStar; 
+      this.btnName="Remove"; 
+    }
+  
+  }
+  AddOrRemove(event?:IEvent):boolean{
+    if(this.favorites){
+      //this.star=this.farStar; 
+      console.log(this.favorites);  
+      this.DeleteFavorite(event); 
+    }
+    else{
+
+      this.AddToFavorite(event); 
+      this.star=this.farStar; 
+
+    }
+    return false; 
+  }
+
+  AddToFavorite(event?:IEvent): boolean{
+    let tempEvent:Event; 
+    tempEvent = new Event(event); 
+    this._eventApiService?.addEventData(tempEvent.event); 
+    console.log(event); 
+    return false;  
+  }
+  DeleteFavorite(event?:IEvent):boolean{
+    this._eventApiService?.delfavoriteData(event); 
+    return false; 
+  }
+
+
+}
 
 
